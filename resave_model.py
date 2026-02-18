ab7c5a551446e19faa404886b16143d71eac17d0
annamariasouri <souri.am@unic.ac.cy>
2026-02-18 15:12:40 +0200

    Remove non-operational files

--- resave_model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Load the existing model using the current environment
-model = joblib.load("final_rf_chl_model_2015_2023.pkl")
-
-# Re-save the model using protocol 4 to improve compatibility
-joblib.dump(model, "new_updated_model.pkl", protocol=4)
-print("Model has been re-saved as new_updated_model.pkl using protocol 4.")
